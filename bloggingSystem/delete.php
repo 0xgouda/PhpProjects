8bd305c906385003e8afaff7a9d2e822faacb416
Ahmed Gouda <119757067+0xgouda@users.noreply.github.com>
2024-10-12 05:18:36 +0300

    Update delete.php

--- bloggingSystem/delete.php
+++ bloggingSystem/delete.php
@@ -2,19 +2,24 @@
 require 'lib/common.php';
 session_start();
 
+$count = count($_POST);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' 
-    && count($_POST) <= 2
-    && count($_POST) >= 1
-    && isset($_POST['article_id'])) {
+    && $count <= 2
+    && $count >= 1
+    && checkVar($_POST['article_id'])) {
 
     $pdo = getPDO();
     $username = getArticleOwner($pdo, $_POST['article_id']);
     if (isLoggedIn() && $username == getUserName()) {
-        if (isset($_POST['comment_id'])) {
+        
+        if (checkVar($_POST['comment_id']) && $count === 2) {
             deleteById($pdo, $_POST['comment_id'], 'comment');
             header('location: view-article.php?article_id=' . $_POST['article_id']);
             exit();
-        } else {
+        } 
+        
+        if ($count === 1 && checkVar('article_id')) {
             deleteById($pdo, $_POST['article_id'], 'article');
         }
     }
